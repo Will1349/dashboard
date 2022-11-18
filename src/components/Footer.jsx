33ecf1b56e8e6b9f6b28598ed0b4c8ee452c8f85
Willams <willabad97@gmail.com>
2022-11-18 18:07:14 -0500

    Componente para mostrar el pie de página

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+    <div className="mt-12">
+        <p className="dark:text-gray-200 text-gray-700 text-center m-10">
+            © 2022 All rights reserved by Willams Abad
+        </p>
+    </div>
+);
+
+export default Footer;
